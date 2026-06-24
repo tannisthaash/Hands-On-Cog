cf8757caeaaeeac8778eae4de28279d842f9cb4f
Tannistha <23052606@kiit.ac.in>
2026-06-25 00:45:04 +0530

    Delete ObserverPatternExample directory

--- ObserverPatternExample/Stock.java
+++
@@ -1,7 +0,0 @@
-package ObserverPatternExample;
-
-public interface Stock {
-    void registerObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers();
-}
